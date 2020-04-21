0b09c917ce7c85c7353224ebd58d8e04bd55ec32
fdezcaminero <fdezcaminero@gmail.com>
2020-04-21 16:08:49 -0400

    Added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true
+
+    validates :name, uniqueness: true
+    validates :email, uniqueness: true
 end
