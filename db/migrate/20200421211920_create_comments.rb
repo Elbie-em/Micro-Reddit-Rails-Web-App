28354ff715f17565515368f08bd76f68bea7ff4e
fdezcaminero <fdezcaminero@gmail.com>
2020-04-21 17:25:46 -0400

    Comment migration

--- db/migrate/20200421211920_create_comments.rb
+++ db/migrate/20200421211920_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+      t.text :body
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
 
