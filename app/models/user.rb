b1dbf719db270073ff0c7d11e26845982f54bddd
elbie-em <emoonga1993@gmail.com>
2020-04-22 00:27:54 +0200

    applied rubocop fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    validates :name, presence: true, uniqueness: true, length: { in: 2..15 }
-    validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { in: 2..15 }
+  validates :email, presence: true, uniqueness: true
 
-    has_many :posts
-    has_many :comments
+  has_many :posts
+  has_many :comments
 end
